2c0b6cc474acb4a896bc7b2ded5873892c897ea6
Shakira Akinleye <shakiraakinleye@gmail.com>
2022-11-21 06:24:20 -0700

    feat: link groups component

---
+++ src/components/Links.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function HelpLinks(){
+    return(
+        <>
+            <a href="www.google.com" className="banner__help__link">Shipping</a>
+            <a href="www.google.com" className="banner__help__link">Return Policy</a>
+            <a href="www.google.com" className="banner__help__link">Maintenance Guide</a>
+            <a href="www.google.com" className="banner__help__link">Contact Us</a>
+            <a href="www.google.com" className="banner__help__link">FAQ</a>
+        </>
+    )
+}
+
+export function SocialLinks(){
+    return(
+        <>
+            <a href="www.google.com" className="footer__social__link">Instagram</a>
+            <a href="www.google.com" className="footer__social__link">Facebook</a>
+            <a href="www.google.com" className="footer__social__link">Twitter</a>
+            <a href="www.google.com" className="footer__social__link">Newsletter</a>
+        </>
+    )
+}
