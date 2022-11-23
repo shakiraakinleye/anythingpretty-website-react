5b2303a72543dad4be7b9dd64da77c9634d521aa
Shakira Akinleye <shakiraakinleye@gmail.com>
2022-11-23 01:45:24 -0700

    style: styled the banners

--- src/components/Banners.js
+++ src/components/Banners.js
@@ -3,20 +3,22 @@ import { HelpLinks } from "./Links";
 
 export function NoticeBanner(){
     return (
-        <div className="banner__announcement">
-            <p className="banner__announcement__content">
+        <div className="bg-black flex justify-center py-3 sm:py-4 border-b-2 border-solid border-b-[#c8c8c8]">
+            <p className="relative w-fit text-white uppercase text-xs">
                 5% discount on all orders above ₦20,000.00 
             </p>
         </div>
     )
 }
-
+ 
 
 export function HelpBanner(){
     return (
-        <div className="banner__help page--width">
-            <div className="banner__help__links w-full py-3 flex flex-wrap justify-between">
-                <HelpLinks />
+        <div className="bg-[#dcdcdc] flex justify-center sm:justify-end px-4 sm:px-8 lg:px-16 border-b-2 border-solid border-b-[#c8c8c8]">
+            <div className="w-full py-3 flex flex-wrap justify-between sm:gap-6 sm:w-max text-xs">
+                <HelpLinks 
+                classes={"text-[#282828] hover:text-[#505050]"}
+                />
             </div>
         </div>
     )
