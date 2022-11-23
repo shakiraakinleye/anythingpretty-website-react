10eb9c89684d881a53167d0d63892d06f6d51345
Shakira Akinleye <shakiraakinleye@gmail.com>
2022-11-23 01:57:11 -0700

    feat: displayed the list of products

--- src/components/ProductSection.js
+++ src/components/ProductSection.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductsContext } from "../App";
+import { Product } from "./Product";
+import "../styles/Product.css"
 
-export function ProductSection({children}){
+export function ProductSection(){
+    function ProductList(){
+        const productsList = useContext(ProductsContext)
+
+        const listItems = productsList.map((product) => {
+            return (
+                <li key={product.id}>
+                    <Product product={product} />
+                </li>
+            )
+        }) 
+        
+        return(
+            <ul className="product__list px-4 sm:px-8 lg:px-16">
+                {listItems}
+            </ul>
+        )
+    }
+
     return(
         <section className="section__product py-10 bg-white text-black">
-            <div className="product__list page--width">
-                {children}
-            </div>
-      </section>
+            <ProductList />
+        </section>
     )
 }
