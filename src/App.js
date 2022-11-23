b2a1de30d63e66888fa3ef8f1b0b9b2951889b14
Shakira Akinleye <shakiraakinleye@gmail.com>
2022-11-23 05:05:37 -0700

    feat: implemented routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import { NoticeBanner, HelpBanner } from './components/Banners';
+import { Navbar } from './components/Navbar';
+import { Homepage } from './pages/Homepage';
+import { Footer } from './components/Footer';
 import './App.css';
+import './index.css';
+
 
 export function App() {
+
   return (
-    <div className="App">
-      
+    <div className="App relative max-w-full tracking-wider">
+        <NoticeBanner />
+        <HelpBanner />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          {/* <Route path="/cart-page" element={<Cartpage />} /> */}
+        </Routes>
+        <Footer />
     </div>
   );
 }
 
- 
