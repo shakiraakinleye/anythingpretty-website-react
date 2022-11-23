0d6548750cd0ba176c34e145eb1adef20c1abc70
Shakira Akinleye <shakiraakinleye@gmail.com>
2022-11-23 01:55:55 -0700

    style: styled the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import fullBrandLogo from "../images/ap-full-logo.jpg";
+// import favoriteIcon from "../images/favorite.png";
+import cartIcon from "../images/cart.png";
 
 export function Navbar(){
     return (
-        <nav className="nav nav--sticky page--width">
-            <div className="nav__content py-3 flex justify-between items-center">
-                <div className="nav__logo">
-                    <a href="index.html">
-                        <img src="images/ap-full-logo.jpg" alt="Brand Logo" />
-                    </a>
+        <nav className="bg-white px-4 sm:px-8 lg:px-16 z-50 sticky top-0 border-b-2 border-solid border-b-[#c8c8c8] ">
+            <div className="py-3 flex justify-between items-center">
+                <div>
+                    <Link to={"/"}>
+                        <img src={fullBrandLogo} alt="Brand Logo" className="w-28 sm:w-40" />
+                    </Link>
                 </div>
 
-                <div className="nav__btns flex">
-                    {/* <button className="btn__favorite">
-                        <img src="images/favorite.png" alt="Favorite List" />
-                    </button>  */}
-
-                    <button className="btn__cart">
-                        <img src="images/cart.png" alt="Cart" />
-                        <p className="cart__count"></p>
-                    </button>
+                <div className="flex">
+                    <Link to={"/cart-page"} className="relative">
+                        <img src={cartIcon} alt="Cart" className="w-6"/>
+                        <p className="bg-[#ca929c] absolute -top-1 -right-1 flex justify-center items-center w-4 h-4 text-xs border-0 border-solid rounded-full  ">1</p>
+                    </Link>
                 </div>
             </div>
         </nav>
