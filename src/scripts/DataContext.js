7b3003a779023f7b0a4befe6ea7907ed9ac8ef71
Shakira Akinleye <shakiraakinleye@gmail.com>
2022-12-09 03:45:55 -0700

    feat: get reviews from JSON

--- src/scripts/DataContext.js
+++ src/scripts/DataContext.js
@@ -2,10 +2,12 @@ import { useState, useEffect, createContext} from "react";
 
 export const ProductsContext = createContext(null)
 export const DeliveryContext = createContext(null)
+export const ReviewsContext = createContext(null)
 
 export function DataContextProvider({children}) {
     const [products, setProducts] = useState([]);
     const [deliveries, setDeliveries] = useState(new Map());
+    const [reviews, setReviews] = useState(new Map())
   
     useEffect(() => {
         fetch("https://shakiraakinleye.github.io/Data/db.json")
@@ -13,11 +15,13 @@ export function DataContextProvider({children}) {
         .then(responseJSON => {
             const products = responseJSON.products;
             const deliveries = responseJSON.deliveries;
-            return {products, deliveries}
+            const reviews = responseJSON.reviews;
+            return {products, deliveries, reviews}
         })
-        .then(({products, deliveries}) => {
+        .then(({products, deliveries, reviews}) => {
             setProducts(products)
             setDeliveries(deliveries)
+            setReviews(reviews)
         })
         .catch(err => console.log(err))
     }, [])
@@ -25,7 +29,9 @@ export function DataContextProvider({children}) {
     return (
         <ProductsContext.Provider value={products}>
             <DeliveryContext.Provider value={deliveries}>
-                {children}
+                <ReviewsContext.Provider value={reviews}>
+                    {children}
+                </ReviewsContext.Provider>
             </DeliveryContext.Provider>
         </ProductsContext.Provider>
     )
